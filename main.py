0a3d351d1a66d60b4c26ab71a6c0cb2127c8f317
KnightofInd <rs1448@srmist.edu.in>
2025-02-13 06:46:50 +0530

    port update

--- main.py
+++ main.py
@@ -200,4 +200,5 @@ async def list_pdfs():
 
 # Run the API server
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
